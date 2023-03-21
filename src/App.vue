990205f57b487803d5e8007ddc23aaf3699b5977
Baby Blue Digital <luke.mcfarland@babyblue.info>
2023-03-21 11:14:55 +0000

    TechPost Component Created

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,49 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import TechPost from './components/TechPost.vue'
+</script>
 
 <template>
-  <div>
-    <ul v-for="post in posts" v-bind:key="post.id">
-      <li>{{ post.title.rendered }}</li>
-      <p>{{ post.content.rendered }}</p>
-    </ul>
-  </div>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+    <TechPost />
+  </main>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      posts: [],
-    };
-  },
-  methods: {
-    async getData() {
-      try {
-        let response = await 
-        fetch("https://wordpress-878924-3326173.cloudwaysapps.com/wp-json/wp/v2/posts");
-        this.posts = await response.json();
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  },
-  created() {
-    this.getData();
-  },
-};
-</script>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/TechPost.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <ul v-for="post in posts" v-bind:key="post.id">
+        <li>{{ post.title.rendered }}</li>
+        <p>{{ post.content.rendered }}</p>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        posts: [],
+      };
+    },
+    methods: {
+      async getData() {
+        try {
+          let response = await 
+          fetch("https://wordpress-878924-3326173.cloudwaysapps.com/wp-json/wp/v2/posts");
+          this.posts = await response.json();
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    },
+    created() {
+      this.getData();
+    },
+  };
+  </script>
