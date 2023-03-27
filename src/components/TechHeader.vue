6acb438cc432012ba3a4a40fd31a1efa5767fd3a
Baby Blue Digital <luke.mcfarland@babyblue.info>
2023-03-27 09:56:49 +0100

    Vuetify icons Added

--- src/components/TechHeader.vue
+++ src/components/TechHeader.vue
@@ -1,45 +1,44 @@
 <template>
-
-<div class="tech_header">
-
-<div>
-<img alt="Vue logo" class="tech_logo" src="../assets/images/logo.svg" width="125" height="125" />
-</div>
-
-<div class="tech_cta">
-
-<div>
-<v-list :items="items"></v-list>
-</div>
-
-<div>
-<v-btn prepend-icon="mdi-vuetify" variant="outlined">Button</v-btn>
-
-</div>
-
-</div>
-
-</div>
-
+    
+    <div class="tech_header">
+        
+        <div>
+            <img alt="Vue logo" class="tech_logo" src="../assets/images/logo.svg" width="125" height="125" />
+        </div>
+        
+        <div class="tech_cta">
+            
+            <div>
+                <v-list :items="items"></v-list>
+            </div>
+            
+            <div>
+                <v-btn prepend-icon="mdi-finance" variant="outlined">Get Started</v-btn>
+            </div>
+            
+        </div>
+        
+    </div>
+    
 </template>
 
 <script>
 export default {
-data: () => ({
-items: [
-{
-title: 'Item #1',
-value: 1,
-},
-{
-title: 'Item #2',
-value: 2,
-},
-{
-title: 'Item #3',
-value: 3,
-},
-],
-}),
+    data: () => ({
+        items: [
+        {
+            title: 'Item #1',
+            value: 1,
+        },
+        {
+            title: 'Item #2',
+            value: 2,
+        },
+        {
+            title: 'Item #3',
+            value: 3,
+        },
+        ],
+    }),
 }
 </script>
