55e2c6eab089a96145be0563a214a0f1cdee3e5a
Baby Blue Digital <luke.mcfarland@babyblue.info>
2023-03-29 13:56:13 +0100

    LevelTech Footer Added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <script setup>
 import TechHeader from './components/TechHeader.vue'
 import TechHome from './components/TechHome.vue'
+import TechFooter from './components/TechFooter.vue'
 </script>
 
 <template>
   <header>
     <TechHeader />
   </header>
-
+  
   <body>
     <TechHome/>
   </body>
-
+  
+  <footer>
+    <TechFooter/>
+  </footer>
+  
 </template>
