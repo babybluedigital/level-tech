9bf8e3510a053c43b383bd47f9a108e7bcefa61c
Baby Blue Digital <luke.mcfarland@babyblue.info>
2023-04-04 14:55:20 +0100

    Home Page Update
    Single Components Created

--- src/components/HomePage/TechHome.vue
+++ src/components/HomePage/TechHome.vue
@@ -1,189 +1,96 @@
-<script setup>
-import TechFooter from '../HomePage/TechHomeHero.vue'
-</script>
-
 <template>
     
     <!-- Hero Section Starts -->
     
-    <template class="hero-section">
-        <TechHomeHero />        
-    </template>
+    <div class="hero-section">
+        <TechHomeHero />     
+    </div>
     
     <!-- Hero Section Ends -->
     
+    
+    
     <!-- Logo Section Starts -->
     
-    <section>
-        
-        <div>
-            
-            <h3>Thousands of players get their rating with LevelTech software</h3>
-            
-        </div>
-        
-        <div>
-            <v-card v-for="(item, index) in logoItems" :key="index" variant="outlined">
-                <v-img :width="300" aspect-ratio="4/3" :src="item.href"></v-img>
-            </v-card>
-        </div>
-        
-    </section>
+    <div class="hero-section">
+        <TechHomelogos />     
+    </div>
     
     <!-- Logo Section Ends-->
     
-    <!-- Column Section Starts -->
-    
-    <section class="column-section">
-        
-        <div>
-            <v-img :width="500" aspect-ratio="4/3" cover src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-img>
-        </div>
-        
-        <div>
-            
-            <h2>Deploy your rating system globally</h2>
-            <h6>Skip the painful setup and development. LevelTech is a global, production-ready system. Skip all the custom dev work and feed your results directly into your ratings.</h6>
-            <v-btn variant="outlined">Button</v-btn>
-            
-        </div>
-        
-    </section>
     
     <!-- Column Section Starts -->
+    
+    <div class="hero-section">
+        <TechHomeCaseStudies />     
+    </div>
+    
+    <!-- Column Section Starts -->
+    
+    <!-- Column Section Starts -->
+    
+    <div class="hero-section">
+        <TechHome4Block />     
+    </div>
+    
+    <!-- Column Section Starts -->
+    
+    
+    
     
     <!-- Testimonial Section Starts -->
     
-    <section class="testimonial-section">
-        
-        <h2>“Time savings is the most important thing for us. It’s been massive. I can record 150 steps in five minutes with Scribe.”</h2>
-        <h6>Mark Williams, CEO England Squash</h6>
-        <v-img :width="300" aspect-ratio="4/3" cover src="src\assets\images\ESLogo.png"></v-img>
-        
-    </section>
+    <div class="hero-section">
+        <TechHomeTestimonial />     
+    </div>
     
     <!-- Testimonial Section Endss -->
     
+    
+    
     <!-- Learn Section Starts -->
     
-    <section class="learn-section">
-        
-        <div>
-            <h2>Want to learn more?</h2>
-            <h6>Learn how LevelTech can deliver your results.</h6>
-        </div>
-        
-        <div>
-            
-            <v-card class="mx-auto" max-width="400">
-                <v-img class="align-end text-white" height="200" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
-                    <v-card-title></v-card-title>
-                </v-img>
-                
-                <v-card-subtitle class="pt-4">
-                    Product Demo
-                </v-card-subtitle>
-                
-                <v-card-text>
-                    <div>See how LevelTech works with a short product demo.</div>
-                </v-card-text>
-                
-                <v-card-actions>
-                    <v-btn color="orange">
-                        Book Now
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-            
-            <v-card class="mx-auto" max-width="400">
-                <v-img class="align-end text-white" height="200" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
-                    <v-card-title></v-card-title>
-                </v-img>
-                
-                <v-card-subtitle class="pt-4">
-                    Case Studies
-                </v-card-subtitle>
-                
-                <v-card-text>
-                    <div>See how LevelTech has helps other sporting codes.</div>
-                </v-card-text>
-                
-                <v-card-actions>
-                    <v-btn color="orange">
-                        Book Now
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-            
-            <v-card class="mx-auto" max-width="400">
-                <v-img class="align-end text-white" height="200" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
-                    <v-card-title></v-card-title>
-                </v-img>
-                
-                <v-card-subtitle class="pt-4">
-                    Resources
-                </v-card-subtitle>
-                
-                <v-card-text>
-                    <div>Learn more with our dedicated resources.</div>
-                </v-card-text>
-                
-                <v-card-actions>
-                    <v-btn color="orange">
-                        Book Now
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-            
-        </div>
-        
-    </section>
+    <div class="hero-section">
+        <TechHomeLearn />     
+    </div>
     
     <!-- Learn Section Ends -->
     
+    
+    
     <!-- Ready Section Starts Here -->
     
-<section>
-
-<div>
-
-    <h2>Ready To Try LevelTech?</h2>
-
-</div>
-
-<div>
-
-    <v-btn variant="outlined">Get Started</v-btn>
-    <v-btn append-icon="mdi-arrow-right" variant="outlined">Request Demo</v-btn>
-
-</div>
-
-</section>
-
+    <div class="hero-section">
+        <TechHomeReady />     
+    </div>
+    
     <!-- Ready Section Ends Here -->
     
 </template>
 
 <script>
-import DunlopLogo from '@/assets/images/DunlopLogo.png';
-import ESLogo from '@/assets/images/ESLogo.png';
-import PSALogo from '@/assets/images/PSALogo.png';
-import SportyLogo from '@/assets/images/SportyLogo.png';
-import SquashSkillsLogo from '@/assets/images/SquashSkillsLogo.png';
 import TechHomeHero from '../HomePage/TechHomeHero.vue';
+import TechHomeCtaBlock from '../HomePage/TechHomeCtaBlock.vue';
+import TechHome4Block from '../HomePage/TechHome4Block.vue';
+import TechHomeDemoBlock from '../HomePage/TechHomeDemoBlock.vue';
+import TechHomeLearn from '../HomePage/TechHomeLearn.vue';
+import TechHomelogos from '../HomePage/TechHomeLogos.vue';
+import TechHomeReady from '../HomePage/TechHomeReady.vue';
+import TechHomeTestimonial from '../HomePage/TechHomeTestimonial.vue';
+import TechHomeCaseStudies from '../HomePage/TechHomeCaseStudies.vue';
 
 export default {
-    data() {
-        return {
-            logoItems: [
-                { href: DunlopLogo },
-                { href: ESLogo },
-                { href: PSALogo },
-                { href: SportyLogo },
-                { href: SquashSkillsLogo },
-            ],
-        };
-    },
-    components: { TechHomeHero }
-};
+    components: {
+        TechHomeHero
+        TechHomeCtaBlock
+        TechHome4Block
+        TechHomeDemoBlock
+        TechHomeLearn
+        TechHomelogos
+        TechHomeReady
+        TechHomeTestimonial
+        TechHomeCaseStudies
+    }
+}
+
 </script>
---
+++ src/components/HomePage/TechHomeReady.vue
@@ -0,0 +1,259 @@
+<style scoped>
+
+section {
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+    gap: 30px;
+    padding: 5% 8%;
+}
+
+.hero-section {
+    min-height: 70vh;
+}
+
+.column-section {
+    flex-direction: row;
+}
+
+.column-section div {
+    flex-direction: column;
+}
+
+.testimonial-section {
+    align-items: flex-start;
+    background-color: #fbf9f4;
+}
+
+.learn-section {
+    flex-direction: column;
+}
+
+.learn-section div:first-of-type {
+    flex-direction: column;
+}
+
+.v-card {
+    flex-direction: column;
+}
+
+div {
+    display: flex;
+    gap: 20px;
+}
+
+h3 {
+    text-align: center;
+}
+
+</style>
+
+<template>
+    
+    <!-- Hero Section Starts -->
+    
+    <section class="hero-section">
+        
+        <div scoped>
+            
+            <h1>Bring It All Together</h1>
+            
+        </div>
+        
+        <div scoped>
+            
+            <h3>Power your sport with the LevelTech rating system.<br>Built with a powerful API, allowing you to reach a global audience.</h3>
+            
+        </div>
+        
+        <div scoped>
+            
+            <v-btn variant="tonal">
+                Button
+            </v-btn>
+            
+            <v-btn variant="outlined">
+                Button
+            </v-btn>
+            
+        </div>
+        
+    </section>
+    
+    <!-- Hero Section Ends -->
+    
+    <!-- Logo Section Starts -->
+    
+    <section>
+        
+        <div>
+            
+            <h3>Thousands of players get their rating with LevelTech software</h3>
+            
+        </div>
+        
+        <div>
+            <v-card v-for="(item, index) in logoItems" :key="index" variant="outlined">
+                <v-img :width="300" aspect-ratio="4/3" :src="item.href"></v-img>
+            </v-card>
+        </div>
+        
+    </section>
+    
+    <!-- Logo Section Ends-->
+    
+    <!-- Column Section Starts -->
+    
+    <section class="column-section">
+        
+        <div>
+            <v-img :width="500" aspect-ratio="4/3" cover src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-img>
+        </div>
+        
+        <div>
+            
+            <h2>Deploy your rating system globally</h2>
+            <h6>Skip the painful setup and development. LevelTech is a global, production-ready system. Skip all the custom dev work and feed your results directly into your ratings.</h6>
+            <v-btn variant="outlined">Button</v-btn>
+            
+        </div>
+        
+    </section>
+    
+    <!-- Column Section Starts -->
+    
+    <!-- Testimonial Section Starts -->
+    
+    <section class="testimonial-section">
+        
+        <h2>“Time savings is the most important thing for us. It’s been massive. I can record 150 steps in five minutes with Scribe.”</h2>
+        <h6>Mark Williams, CEO England Squash</h6>
+        <v-img :width="300" aspect-ratio="4/3" cover src="src\assets\images\ESLogo.png"></v-img>
+        
+    </section>
+    
+    <!-- Testimonial Section Endss -->
+    
+    <!-- Learn Section Starts -->
+    
+    <section class="learn-section">
+        
+        <div>
+            <h2>Want to learn more?</h2>
+            <h6>Learn how LevelTech can deliver your results.</h6>
+        </div>
+        
+        <div>
+            
+            <v-card class="mx-auto" max-width="400">
+                <v-img class="align-end text-white" height="200" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
+                    <v-card-title></v-card-title>
+                </v-img>
+                
+                <v-card-subtitle class="pt-4">
+                    Product Demo
+                </v-card-subtitle>
+                
+                <v-card-text>
+                    <div>See how LevelTech works with a short product demo.</div>
+                </v-card-text>
+                
+                <v-card-actions>
+                    <v-btn color="orange">
+                        Book Now
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+            
+            <v-card class="mx-auto" max-width="400">
+                <v-img class="align-end text-white" height="200" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
+                    <v-card-title></v-card-title>
+                </v-img>
+                
+                <v-card-subtitle class="pt-4">
+                    Case Studies
+                </v-card-subtitle>
+                
+                <v-card-text>
+                    <div>See how LevelTech has helps other sporting codes.</div>
+                </v-card-text>
+                
+                <v-card-actions>
+                    <v-btn color="orange">
+                        Book Now
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+            
+            <v-card class="mx-auto" max-width="400">
+                <v-img class="align-end text-white" height="200" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
+                    <v-card-title></v-card-title>
+                </v-img>
+                
+                <v-card-subtitle class="pt-4">
+                    Resources
+                </v-card-subtitle>
+                
+                <v-card-text>
+                    <div>Learn more with our dedicated resources.</div>
+                </v-card-text>
+                
+                <v-card-actions>
+                    <v-btn color="orange">
+                        Book Now
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+            
+        </div>
+        
+    </section>
+    
+    <!-- Learn Section Ends -->
+    
+    <!-- Ready Section Starts Here -->
+    
+<section>
+
+<div>
+
+    <h2>Ready To Try LevelTech?</h2>
+
+</div>
+
+<div>
+
+    <v-btn variant="outlined">Get Started</v-btn>
+    <v-btn append-icon="mdi-arrow-right" variant="outlined">Request Demo</v-btn>
+
+</div>
+
+</section>
+
+    <!-- Ready Section Ends Here -->
+    
+</template>
+
+<script>
+import DunlopLogo from '@/assets/images/DunlopLogo.png';
+import ESLogo from '@/assets/images/ESLogo.png';
+import PSALogo from '@/assets/images/PSALogo.png';
+import SportyLogo from '@/assets/images/SportyLogo.png';
+import SquashSkillsLogo from '@/assets/images/SquashSkillsLogo.png';
+
+export default {
+    data() {
+        return {
+            logoItems: [
+            { href: DunlopLogo },
+            { href: ESLogo },
+            { href: PSALogo },
+            { href: SportyLogo },
+            { href: SquashSkillsLogo },
+            ],
+        };
+    },
+};
+</script>
